a4e9612c4779279bdec81c2692f1039bd2988866
AbdelrahmanOthmanHelal <ao.helal@outlook.com>
2017-09-12 23:13:43 +0200

    - Adding a function to get the active port numbers - Correcting some comments

--- InternetConnection.cs
+++ InternetConnection.cs
@@ -1,52 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
+using System.IO;
 using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace WinToolkit
 {
-	public class InternetConnectionCheker
-	{
-		private static bool InternalIsConnectionAvailable(int timeout)
-		{
-			const string host = "google.com";
+    public class InternetConnectionCheker
+    {
+        private static bool InternalIsConnectionAvailable(int timeout)
+        {
+            const string host = "google.com";
 
-			var ping = new Ping();
-			var buffer = new byte[32];
-			var pingOptions = new PingOptions();
+            var ping = new Ping();
+            var buffer = new byte[32];
+            var pingOptions = new PingOptions();
 
-			try
-			{
-				var reply = ping.Send(host, timeout, buffer, pingOptions);
-				return (reply != null && reply.Status == IPStatus.Success);
-			}
-			catch (Exception)
-			{
-				return false;
-			}
-		}
+            try
+            {
+                var reply = ping.Send(host, timeout, buffer, pingOptions);
+                return (reply != null && reply.Status == IPStatus.Success);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
 
-		public static bool IsConnectionAvailable(int retryCount = 0, int timeoutInMSec = 3000)
-		{
-			bool status = false;
+        public static bool IsConnectionAvailable(int retryCount = 0, int timeoutInMSec = 3000)
+        {
+            bool status = false;
             do
             {
                 status = InternalIsConnectionAvailable(timeoutInMSec);
             } while (retryCount-- > 0);
 
-			return status;
-		}
-	}
-    public class NetworkInterfaceCardAttributes
+            return status;
+        }
+    }
+    public class NetworkInterfaceAttributes
     {
         public static string GetMacAddress()
         {
             // Looping through all interfaces to get the active one ..
             foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
             {
-                // Only consider Ethernet network interfaces
+                // Considering active Ethernet and WiFi network interface/adapter
                 if ((nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet ||
                     nic.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
                     && (nic.OperationalStatus == OperationalStatus.Up))
@@ -55,6 +55,37 @@ namespace WinToolkit
                 }
             }
             return null;
+        }
+        public static List<int> GetActivePortNumbers()
+        {
+            List<int> portNumbers = new List<int>();
+
+            using (Process process = new Process())
+            {
+                ProcessStartInfo processInfo = new ProcessStartInfo();
+                processInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                processInfo.RedirectStandardError = true;
+                processInfo.RedirectStandardOutput = true;
+                processInfo.RedirectStandardInput = true;
+                processInfo.UseShellExecute = false;
+                processInfo.Arguments = "-a -n -o";
+                processInfo.FileName = "netstat.exe";
+                processInfo.CreateNoWindow = true;
+                process.StartInfo = processInfo;
+                process.Start();
+
+                StreamReader stdOutput = process.StandardOutput;
+
+                // Splitting the rows of the result into a grid-like style
+                string[] portsResultSet = Regex.Split(stdOutput.ReadToEnd(), "\r\n");
+                foreach (string portRecord in portsResultSet)
+                {
+                    string[] tokens = Regex.Split(portRecord, "\\s+");
+                    if (tokens.Length > 4 && (tokens[1].Equals("UDP") || tokens[1].Equals("TCP")))
+                        portNumbers.Add(int.Parse(Regex.Replace(tokens[2], @"\[(.*?)\]", "1.1.1.1").Split(':')[1]));
+                }
+            }
+            return portNumbers;
         }
     }
 }
